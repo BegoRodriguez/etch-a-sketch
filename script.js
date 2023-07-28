1e0dcf49554d1be42135e6edb32cb91c34e96a62
BegoRodriguez <136585766+BegoRodriguez@users.noreply.github.com>
2023-07-28 10:09:23 +0200

    Add the files with just the container done

---
+++ script.js
@@ -0,0 +1,15 @@
+
+   
+   const grid = document.querySelector('#myGrid');
+
+   // The positions?? // The sizes??
+   // Dejo unos margenes en la pantalla, por ejemplo el container es el 90% de vw cada uno de estos sería width 90vw/16
+   // Necesitaría que fuera cuadrado?
+   for(let i=0;i<16;i++){
+      for(let i=0;i<16;i++){
+      console.log("hola " + i);
+      }
+   }
+   
+
+ 
